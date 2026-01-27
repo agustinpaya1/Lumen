dd4c525914b5a17877611dc613458de2a2dae966
Agustín <apa200310@gmail.com>
2026-01-27 16:55:52 +0100

    Refactor to implement bussiness quality code.

--- api/apps/photos/api.py
+++ api/apps/photos/api.py
@@ -1,29 +1,15 @@
 # api/apps/photos/api.py
 from ninja import Router, File
 from ninja.files import UploadedFile
-from .models import Photo
-from .providers.gdrive import GoogleDriveProvider
+from .services import PhotoService
 
 router = Router()
 
 @router.post("/upload")
 def upload_photo(request, file: UploadedFile = File(...)):
-    # 1. Instanciar el provider (Conecta con Google)
-    provider = GoogleDriveProvider()
-    
-    # 2. Subir a Drive (La magia ocurre aquí)
-    # file.file es el objeto en memoria (BytesIO)
-    drive_id = provider.upload_file(
-        file.file, 
-        file.name, 
+    service_response = PhotoService.upload_photo(
+        file.file,
+        file.name,
         file.content_type
     )
-    
-    # 3. Guardar referencia en nuestra DB local
-    photo = Photo.objects.create(drive_file_id=drive_id)
-    
-    return {
-        "success": True, 
-        "photo_id": str(photo.id),
-        "drive_id": drive_id
-    }
+    return service_response
