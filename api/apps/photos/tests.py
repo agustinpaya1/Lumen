022ed6b6bb9274e1ba95f1936500facba57ca5aa
Agustín <apa200310@gmail.com>
2026-01-31 18:18:17 +0100

    errased

--- api/apps/photos/tests.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-from .models import Photo
-
-
-class PhotoTestCase(TestCase):
-    def test_photo_creation(self):
-        """Test that a photo can be created (logic verification)"""
-        self.assertTrue(True)
